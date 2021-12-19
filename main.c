26befec31d08f2fea129615444e7e885a45bd7d0
abdelrahman mohamed mohamed abdelmotlb <abdelmotlb10@gmail.com>
2021-12-19 21:33:28 +0200

    1

--- main.c
+++ main.c
@@ -7,12 +7,13 @@ int main(){
 \\ stoping and typing into it
 void gotoxy(int x,int y)
 {
+    COORD coord= {0,0};
     coord.X=x;
     coord.Y=y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
 
-
+\\ 
 
 
 
